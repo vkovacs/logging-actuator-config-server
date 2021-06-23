393178bb8bd6e54500f490a77963a7cee07d4a27
Viktor Kovacs <viktor.kovacs@siemens.com>
2021-06-23 08:38:30 +0200

    init config server

---
+++ src/main/java/com/siemens/loggingconfigserver/LoggingConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.siemens.loggingconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class LoggingConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoggingConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/siemens/loggingconfigserver/LoggingConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siemens.loggingconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
